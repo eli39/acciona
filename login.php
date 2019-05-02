b793364dea46c8618d82a36e9374a53300bec468
Eli Álvarez <informaticalvarez1@gmail.com>
2019-05-02 15:22:07 -0500

    Cambia placeholder input login contrasena

--- login.php
+++ login.php
@@ -44,7 +44,7 @@
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password">
+        <input type="password" class="form-control" placeholder="Contraseña">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
